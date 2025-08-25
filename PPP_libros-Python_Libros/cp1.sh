bdada533cc2ba2d16a41711202e42587bd6eb1cd
matecom57 <matecom57@gmail.com>
2025-08-25 04:39:24 -0600

    Test comité

---
+++ PPP_libros-Python_Libros/cp1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+b1="2015_Book_AnIntroductionToPythonAndCompu.pdf"
+b2="2016_Book_AnIntroductionToStatisticsWith.pdf"
+b3="2016_Book_APrimerOnScientificProgramming.pdf"
+
+pat1="/mnt/datos/santosg/Libros_TODOS_antonieta/PPP_libros/Python_Libros/"
+
+cp $pat1$b1 . 
+cp $pat1$b2 .
+cp $pat1$b3 .
+
+
+
